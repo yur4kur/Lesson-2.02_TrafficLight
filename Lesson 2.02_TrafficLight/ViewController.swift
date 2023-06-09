46ba429b625fa529b643a77b2af3cf42c1dfe0d5
Iurii Kurinnoi <yur4kur@icloud.com>
2023-06-09 21:14:50 +0400

    Basic code for outlets and button.

--- Lesson 2.02_TrafficLight/ViewController.swift
+++ Lesson 2.02_TrafficLight/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redSignalView: UIView!
+    @IBOutlet var yellowSignalView: UIView!
+    @IBOutlet var greenSignalView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        signalViewSetup(redSignalView)
+        signalViewSetup(yellowSignalView)
+        signalViewSetup(greenSignalView)
     }
 
-
+    @IBAction func startButtonDidTap() {
+        startButton.setTitle("NEXT", for: .normal)
+        redSignalView.alpha = 1
+    }
+    
+    private func signalViewSetup(_ view: UIView) {
+        view.layer.cornerRadius = 70
+        view.alpha = 0.3
+    }
 }
 
