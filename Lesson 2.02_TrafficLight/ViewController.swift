163b7edd6158a5e9a3ce376b825b71b18ac33ed1
Iurii Kurinnoi <yur4kur@icloud.com>
2023-06-10 15:32:06 +0400

    Minor style changes.

--- Lesson 2.02_TrafficLight/ViewController.swift
+++ Lesson 2.02_TrafficLight/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        
         for view in trafficLightViews {
             signalViewSetup(view)
         }
